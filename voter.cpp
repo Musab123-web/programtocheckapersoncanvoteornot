718744e6582832d7b1ac4effabbfd54b32f304e1
Musab123-web <84624775+Musab123-web@users.noreply.github.com>
2021-06-27 13:45:18 +0530

    created by Musab
    prints on the monitor whether a person can vote or not on entering the age

---
+++ voter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter the age of the user:";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"\nYou are eligible for voting";
+	}
+	else
+	{
+		cout<<"\nYou are not eligible for voting";
+	}
+	return 0;
+}
